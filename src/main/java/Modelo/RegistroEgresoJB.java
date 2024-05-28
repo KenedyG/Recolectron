6d0271654b97818f079bef5a2069be47f5a64767
SeluneElegy <trollzorFernandez@gmail.com>
2024-05-28 11:24:13 -0600

    Login JavaBean

---
+++ src/main/java/Modelo/RegistroEgresoJB.java
@@ -0,0 +1,70 @@
+package Modelo;
+
+import java.io.*;
+
+public class RegistroEgresoJB implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int idRegistroEgreso;
+    private String nombre;
+    private String matricula;
+    private String materia;
+    private String nivelacademico;
+    private String donativo;
+    private String fecha;
+    private int idelectronico;
+
+    public RegistroEgresoJB() {
+
+    }
+    public RegistroEgresoJB(int idelectronico, String nombre, String matricula, String materia, String nivelacademico, String donativo, String fecha, int idRegistroEgreso){
+
+    }
+    public int getIdRegistroEgreso() {
+        return idRegistroEgreso;
+    }
+    public void setIdRegistroEgreso(int idRegistroEgreso) {
+        this.idRegistroEgreso = idRegistroEgreso;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public String getMateria() {
+        return materia;
+    }
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+    public String getNivelacademico() {
+        return nivelacademico;
+    }
+    public void setNivelacademico(String nivelacademico) {
+        this.nivelacademico = nivelacademico;
+    }
+    public String getDonativo() {
+        return donativo;
+    }
+    public void setDonativo(String donativo) {
+        this.donativo = donativo;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public int getIdelectronico() {
+        return idelectronico;
+    }
+    public void setIdelectronico(int idelectronico) {
+        this.idelectronico = idelectronico;
+    }
+}
