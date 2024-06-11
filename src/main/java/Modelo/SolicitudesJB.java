45eabb407e3d1c6f8c2c26d1330da44a8dec389a
KenedyG <kenedygarcia43@gmail.com>
2024-06-11 15:51:09 -0600

    Cambios de SVSolicitudHi

--- src/main/java/Modelo/SolicitudesJB.java
+++ src/main/java/Modelo/SolicitudesJB.java
@@ -2,51 +2,42 @@ package Modelo;
 
 import java.io.*;
 import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
 
 public class SolicitudesJB implements Serializable {
     private static final long serialVersionUID = 1L;
-    private int idSolicitud;
     private int noequipo;
-    private Timestamp fechaSolicitud;
-    private Timestamp fechaUso;
+    private Date fechaSolicitud;
+    private Date fechaUso;
     private String status;
     private int idalumno;
 
     public SolicitudesJB() {}
 
-    public SolicitudesJB(int idSolicitud, int noequipo, Timestamp fechaSolicitud, Timestamp fechaUso, String status, int idalumno) {
-        this.idSolicitud = idSolicitud;
-        this.noequipo = noequipo;
-        this.fechaSolicitud = fechaSolicitud;
-        this.fechaUso = fechaUso;
-        this.status = status;
-        this.idalumno = idalumno;
-    }
-    public int getIdSolicitud() {
-        return idSolicitud;
+    public SolicitudesJB(int noequipo, Date fechaSolicitud, Date fechaUso, String status, int idSolicitudes) {
     }
 
-    public void setIdSolicitud(int idSolicitud) {
-        this.idSolicitud = idSolicitud;
+    public SolicitudesJB(int noequipo, String fechaSolicitud, Date fechaUso, String status, int idAlumno) {
     }
+
     public int getNoequipo() {
         return noequipo;
     }
     public void setNoequipo(int Noequipo) {
         this.noequipo = Noequipo;
     }
-    public Timestamp getFechaSolicitud() {
+    public Date getFechaSolicitud() {
         return fechaSolicitud;
     }
-    public void setFechaSolicitud(Timestamp fechaSolicitud) {
+    public void setFechaSolicitud(Date fechaSolicitud) {
         this.fechaSolicitud = fechaSolicitud;
     }
-    public Timestamp getFechaUso() {
+    public Date getFechaUso() {
         return fechaUso;
     }
-    public void setFechaUso(Timestamp fechaUso) { this.fechaUso = fechaUso;}
+    public Date setFechaUso(Date fechaUso) {
+        this.fechaUso = fechaUso;
+        return fechaUso;
+    }
     public String getStatus() {
         return status;
     }
